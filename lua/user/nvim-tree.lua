e73742e0e5c000531d9651467d47a3f870fc108c
leff32 <leff32@gmail.com>
2022-07-16 21:23:37 +1000

    nvim-tree setup update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,54 +1,24 @@
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-    }
-}
--- TODO: git icons not showing
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, 'nvim-tree.config')
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
+  open_on_setup = true,
   ignore_ft_on_setup = {
     "startify",
     "dashboard",
     "alpha",
   },
---  auto_close = false,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
---  update_to_buf_dir = {
---    enable = true,
---    auto_open = true,
---  },
+  hijack_unnamed_buffer_when_opening = true,
+  hijack_cursor = true,
+  sync_root_with_cwd = true,
+  hijack_directories = {
+   enable = true,
+   auto_open = true,
+  },
   diagnostics = {
     enable = true,
     icons = {
@@ -58,45 +28,46 @@ nvim_tree.setup {
       error = "",
     },
   },
+  renderer = {
+    full_name = true,
+    highlight_opened_files = "all",
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        }
+      }
+    }
+  },
   update_focused_file = {
     enable = true,
-    update_cwd = true,
     ignore_list = {},
-  },
-  -- system_open = {
-  --   cmd  = nil,
-  --   args = {}
-  -- },
-  filters = {
-    dotfiles = true,
-    custom = {}
   },
   git = {
     enable = true,
     ignore = true,
     timeout = 1000,
   },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    -- auto_resize = true,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
+  actions = {
+    open_file = {
+      quit_on_open = false,
     },
-    number = false,
-    relativenumber = false,
-  },
---   quit_on_open = 0,
---   git_hl = 1,
---   disable_window_picker = 0,
---   root_folder_modifier = ":~",
+  }
 }
 
 local view_status_ok, nvim_tree_view = pcall(require, "nvim-tree.view")
@@ -116,8 +87,8 @@ _M.toggle_tree = function()
     bufferline_state.set_offset(0)
   else
     bufferline_state.set_offset(31)
---  nvim_tree.find_file(true)
-    nvim_tree.open()
+    nvim_tree.find_file(true)
+    -- nvim_tree.open()
   end
 end
 
