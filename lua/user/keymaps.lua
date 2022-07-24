c772b8471be897eae55e3680384c8f1bf1db88c9
leff32 <leff32@gmail.com>
2022-07-24 20:57:19 +1000

    updated keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -23,10 +23,10 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<S-Up>", ":resize -2<CR>", opts)
-keymap("n", "<S-Down>", ":resize +2<CR>", opts)
-keymap("n", "<S-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<S-Up>", ":resize +2<CR>", opts)
+keymap("n", "<S-Down>", ":resize -2<CR>", opts)
+keymap("n", "<S-Left>", ":vertical resize +2<CR>", opts)
+keymap("n", "<S-Right>", ":vertical resize -2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":BufferNext<CR>", opts)
