ec9a117907cdce4b0a6d6d3623ce53449679a198
leff32 <leff32@gmail.com>
2022-08-08 21:45:47 +1000

    treesitter update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,20 +1,24 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not  status_ok then
-  return
+if not status_ok then
+	return
 end
 
-configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false,
-  ignore_install = { "" }, -- list of parsers to ignore installing
-  autopairs = { enable = true },
-  highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = { "" }, -- list of language that will be disabled
-      additional_vim_regex_highlighting = true
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  autotag = {enable = true},
-  context_commentstring = {enable = true, enable_autocmd =false, config = {javascriptreact = {style_element = '{/*%s*/}'}}},
-  refactor = {highlight_definitions = {enable = true}}
-}
+configs.setup({
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	sync_install = false,
+	ignore_install = { "" }, -- list of parsers to ignore installing
+	autopairs = { enable = true },
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+	autotag = { enable = true },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	-- context_commentstring = {enable = true, enable_autocmd =false, config = {javascriptreact = {style_element = '{/*%s*/}'}}},
+	refactor = { highlight_definitions = { enable = true } },
+})
